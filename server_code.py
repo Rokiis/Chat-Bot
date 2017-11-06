e9f97cfb4df123af7ac867c0802d05bac07028d5
Patryk Kluzik (kluzikp) <kluzikp@coventry.ac.uk>
2017-11-06 15:14:54 +0000

    Update server_code.py

--- server_code.py
+++ server_code.py
@@ -1,19 +1,6 @@
 import socket
 import time
-import re
-import random
- # Sentences we'll respond with if the user greeted us
-GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up",)
-GREETING_RESPONSES = ["'sup bro", "hey", "*nods*", "hey you get my snap?"]
-
-def check_for_greeting(sentence):
-    """If any of the words in the user's input was a greeting, return a greeting response"""
-    wordList = re.sub("[^\w]", " ",  sentence).split()
-    for word in wordList:
-        if word.lower() in GREETING_KEYWORDS:
-            returnMess = random.choice(GREETING_RESPONSES)
-            return returnMess
-
+ 
  
 def Main():
     #Give ChatBot server an IP address and port
@@ -32,15 +19,13 @@ def Main():
     while True:
                 #Receive info from client
                 receiveMess = conn.recv(1024).decode()
-                
                 #if no info from client end loop
                 if not receiveMess:
                                     break
                 #Print info from client
                 print ("Message from User to Chatbot : " + str(receiveMess))
                 #set return message
-                #returnMess = "This is the return message"
-                returnMess = check_for_greeting(receiveMess)
+                returnMess = "This is the return message"
                 conn.send(returnMess.encode())                             
     conn.close()                
 if __name__ == '__main__':
