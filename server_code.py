f384eef37fc02ad9078250b320cea53199ba65cf
Rokas Navickas (navickar) <navickar@coventry.ac.uk>
2017-11-20 17:48:33 +0200

    Latest changes

--- server_code.py
+++ server_code.py
@@ -7,22 +7,44 @@ from discordx import discord
 import asyncio
 import aiohttp
 import websockets#Do not edit anything above this unless it is necessary for your code to work.
-imv = imdb.IMDb() #imdb connection linking.
+mv = imdb.IMDb() #imdb connection linking.
 client = discord.Client() #discord api connection linking
 
 
- # Sentences we'll respond with if the user greeted us
-GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up",) #this has to be put into database.
-GREETING_RESPONSES = ["'sup bro", "hey", "*nods*", "hey you get my snap?"]#this one had to be put into db too.
-
-def check_for_greeting(sentence): #also, this function has to be called from different file(database of all Q/A)
+#KEYWORD FILTERING
+GREETING_KEYWORDS = ("hello",  "hi", "greetings", "sup","hey")
+GREETING_RESPONSES = ["'sup bro", "hey", "*nods*", "hello", "hi","nice to hear from you"]#this one had to be put into db too.
+def check_for_greeting(sentence): 
 	"""If any of the words in the user's input was a greeting, return a greeting response"""
 	wordList = re.sub("[^\w]", " ",  sentence).split()
 	for word in wordList:
 		if word.lower() in GREETING_KEYWORDS:
 			returnMess = random.choice(GREETING_RESPONSES)
 			return returnMess
+			
+PRIMARY_QUESTION_KEYWORDS = ("how", "are","is")
+SECONDARY_QUESTION_KEYWORDS = ("you","everything","are")
+FINAL_QUESTION_KEYWORDS = ("good", "alright"," fine", "you")
+QUESTION_RESPONSES = ["I'm good, thanks for asking", "Not so bad", "Pretty good", "So far so good", "Honestly, life is going better that ever", "I've been struggling for a while, but now im good"]
+def check_for_questions(sentencex): #also, this function has to be called from different file(database of all Q/A)
+	"""If any of the words in the user's input was a greeting, return a greeting response"""
+	wordList = re.sub("[^\w]", " ",  sentencex).split()
+	returnMess = ("I did'nt understood you question, can you please give me a more simple one? Im a workpiece of a first year students. Dont exepect too much from me.")
+	for word1 in wordList:
+		if word1 in PRIMARY_QUESTION_KEYWORDS:
+			for word2 in wordList:
+				if word2 in SECONDARY_QUESTION_KEYWORDS:
+					for word3 in wordList:
+						if word3 in FINAL_QUESTION_KEYWORDS:
+							returnMess = random.choice(QUESTION_RESPONSES)
+	return returnMess
 
+
+def lowercasing(sentence):
+	print("LOWERCASING")
+	words = sentence.split()
+	words = [element.lower() for element in words]
+	return words
 
 
 @client.event #Discord API connection.
@@ -35,21 +57,21 @@ async def on_ready():
 
 @client.event #Test stuff. This will stay here as an example and debugging.
 async def on_message(message):	
-	print(message.content)
-	if message.content.startswith('!test'):
-		counter = 0
-		tmp = await client.send_message(message.channel, 'Calculating messages...')
-		async for log in client.logs_from(message.channel, limit=100):
-			if log.author == message.author:
-				counter += 1
-
-		await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-	elif message.content.startswith('!sleep'):
-		await asyncio.sleep(5)
-		await client.send_message(message.channel, 'Done sleeping')
-	if message.content in GREETING_KEYWORDS:
-		response=(check_for_greeting(message.content))
-	await client.send_message(message.channel, response)
+	if not message.author.bot:
+		print(message.content)
+		if message.content in GREETING_KEYWORDS:
+			response=(check_for_greeting(message.content))
+		elif message.content.endswith('?'):
+			print("QUESTION")
+			message.content.replace("?","")
+			response = (check_for_questions(message.content))
+		else:
+			print("MOVIE")		
+			await client.send_message(message.channel, "I'll take a look")
+			movie = mv.search_movie(message.content)[0] # a Movie instance.
+			m = mv.get_movie(movie.movieID)
+			response =("Is that the movie we are talking about? - " + str(movie) + "?")
+		await client.send_message(message.channel, response)
 
 
 #Do not edit anything under this one. 
