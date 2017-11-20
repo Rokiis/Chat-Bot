8ef2b1dc38f215b430baff53a0916c26e7c4c449
Rokas Navickas (navickar) <navickar@coventry.ac.uk>
2017-11-21 01:46:36 +0200

    Canges
    Fixed the code. Made greetings, questions, keywords, filtering work properly, implemented memory to a chatbot, safety checks on movies.

--- server_code.py
+++ server_code.py
@@ -9,44 +9,57 @@ import aiohttp
 import websockets#Do not edit anything above this unless it is necessary for your code to work.
 mv = imdb.IMDb() #imdb connection linking.
 client = discord.Client() #discord api connection linking
+memory = []
 
-
+AGGREE_KEYOWORDS = ('yes','y','yeah')
+FILTERING_KEYWORDS = ('summary','release','date','director')
 #KEYWORD FILTERING
-GREETING_KEYWORDS = ("hello",  "hi", "greetings", "sup","hey")
-GREETING_RESPONSES = ["'sup bro", "hey", "*nods*", "hello", "hi","nice to hear from you"]#this one had to be put into db too.
+GREETING_KEYWORDS = ('hello',  'hi', 'greetings', 'sup','hey')
+GREETING_RESPONSES = ["'sup bro",":kappa:" "hey", "*nods*", "hello", "hi","nice to hear from you"]
 def check_for_greeting(sentence): 
-	"""If any of the words in the user's input was a greeting, return a greeting response"""
 	wordList = re.sub("[^\w]", " ",  sentence).split()
 	for word in wordList:
-		if word.lower() in GREETING_KEYWORDS:
+		if word in GREETING_KEYWORDS:
 			returnMess = random.choice(GREETING_RESPONSES)
 			return returnMess
 			
-PRIMARY_QUESTION_KEYWORDS = ("how", "are","is")
-SECONDARY_QUESTION_KEYWORDS = ("you","everything","are")
-FINAL_QUESTION_KEYWORDS = ("good", "alright"," fine", "you")
+QUESTION_KEYWORDS = ('how','are','you','good','alright','hows','it','going','ok')
 QUESTION_RESPONSES = ["I'm good, thanks for asking", "Not so bad", "Pretty good", "So far so good", "Honestly, life is going better that ever", "I've been struggling for a while, but now im good"]
-def check_for_questions(sentencex): #also, this function has to be called from different file(database of all Q/A)
-	"""If any of the words in the user's input was a greeting, return a greeting response"""
-	wordList = re.sub("[^\w]", " ",  sentencex).split()
-	returnMess = ("I did'nt understood you question, can you please give me a more simple one? Im a workpiece of a first year students. Dont exepect too much from me.")
-	for word1 in wordList:
-		if word1 in PRIMARY_QUESTION_KEYWORDS:
-			for word2 in wordList:
-				if word2 in SECONDARY_QUESTION_KEYWORDS:
-					for word3 in wordList:
-						if word3 in FINAL_QUESTION_KEYWORDS:
-							returnMess = random.choice(QUESTION_RESPONSES)
+def check_for_questions(sentence): #also, this function has to be called from different file(database of all Q/A)
+	wordList = re.sub("[^\w]", " ",  sentence).split()
+	returnMess = ("I did'nt understood your question, can you please give me a more simple one? Im a workpiece of a first year students. Dont exepect too much from me.")
+	y=0
+	while y < len(wordList)-1:
+		if wordList[y] in QUESTION_KEYWORDS and wordList[y+1] in QUESTION_KEYWORDS and wordList[y+1] in QUESTION_KEYWORDS:
+			returnMess = random.choice(QUESTION_RESPONSES)
+		y=y+1
 	return returnMess
 
 
 def lowercasing(sentence):
-	print("LOWERCASING")
-	words = sentence.split()
+	words = re.sub("[^\w]", " ",  sentence).split()
 	words = [element.lower() for element in words]
-	return words
+	return " ".join(words)
 
+def filter_on_message(message):
+	wordList = re.sub("[^\w]", " ",  message).split()
+	for word in wordList:
+		if word in FILTERING_KEYWORDS:
+			returnMess = word 
+			return returnMess
 
+def filter_on_movie(message):
+	lists = re.sub("[^\w]", " ",  message).split()
+	i = 0
+	while i < len(lists):
+		if lists[i] != "movie":
+			lists.remove(lists[i])
+			i=0
+		elif lists[i] == "movie":
+			lists.remove("movie")
+			if "of" in lists:
+				lists.remove("of")
+			return " ".join(lists)
 @client.event #Discord API connection.
 async def on_ready():
 	print('Logged in as')
@@ -56,21 +69,47 @@ async def on_ready():
 
 
 @client.event #Test stuff. This will stay here as an example and debugging.
-async def on_message(message):	
+async def on_message(message):
 	if not message.author.bot:
+		msg = message.content
+		memory.append(message.content)
+		message.content = lowercasing(msg)
 		print(message.content)
-		if message.content in GREETING_KEYWORDS:
+		if message.content.startswith('test'):
+			response = (":scream:")
+		elif "movie" in message.content:
+			moviefound = filter_on_movie(message.content)
+			memory.append(moviefound)
+			movie = mv.search_movie(moviefound)[0] # a Movie instance.
+			m = mv.get_movie(movie.movieID)
+			if message.content in FILTERING_KEYWORDS:
+				filteredkeyword = (filter_on_message)
+				response = ("I wish i could give you the " + str(filteredkeyword) + " of the movie " + str(m))
+			else:
+				response = ("Is this the movie you have asked about?" + str(m))
+
+		elif message.content in AGGREE_KEYOWORDS:
+			print("AGGREED")
+			print(str(memory[-2] + "MEMORY LAST"))
+			movie = mv.search_movie(memory[-2])[0] # a Movie instance.
+			m = mv.get_movie(movie.movieID)
+			response = ("What would you want to know about this movie? -" + str(m) + ". Write the thing you want to know only please.")
+		elif message.content in FILTERING_KEYWORDS:
+			print("FILTERING")
+			keyword = filter_on_message(memory[-1])
+			print("MOVIE MEMORY" + str(memory[-3]))
+			movie = mv.search_movie(memory[-3])[0] # a Movie instance.
+			m = mv.get_movie(movie.movieID)
+			response = ("I cant give the " + str(keyword) + " of movie - " + str(m) + " yet. I'm sorry... :weary: ")
+		elif message.content in GREETING_KEYWORDS:
+			print("GREETING")
 			response=(check_for_greeting(message.content))
-		elif message.content.endswith('?'):
+			print(response)
+		elif msg.endswith('?'):
 			print("QUESTION")
 			message.content.replace("?","")
 			response = (check_for_questions(message.content))
-		else:
-			print("MOVIE")		
-			await client.send_message(message.channel, "I'll take a look")
-			movie = mv.search_movie(message.content)[0] # a Movie instance.
-			m = mv.get_movie(movie.movieID)
-			response =("Is that the movie we are talking about? - " + str(movie) + "?")
+			print(response)
 		await client.send_message(message.channel, response)
 
 
