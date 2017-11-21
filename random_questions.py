76892d8d00dacc82f395e041bb9e875dd30aa0d9
Qais Khan (khanq7) <khanq7@coventry.ac.uk>
2017-11-21 01:15:33 +0000

    Update random_questions.py

--- random_questions.py
+++ random_questions.py
@@ -27,4 +27,29 @@ def random_question_filter(message):
         return "I dont talk about politics ONLY MOVIES SIR!"
     elif message == "what's the weather":
         return "How do you expect me to know im not a weather forecast"
+    elif message == "who are you":
+        return "I currently have no name but just called me Robo"
+    elif message == "what is the age of the universe":
+        return "The age of the Universe is 13.82 billion years!"
+    elif message == "what is your favorite color?":
+        return "My favorite color is Blue!"
+    elif message == "what was your favorite subject at school":
+        return "I'm only a :robot: you know"
+    elif message == "what school did you go to?":
+        return "I'm a robot! Who needs school"
+    elif message == "what type of music do you like":
+        return "My favorite type of music is Electronic!!! :robot:"
+    elif message == "are you married":
+        return "I'm a :robot: i don't need these things"
+    elif message == "what do you do for fun":
+        return "I enjoy running through the database of IMDb!"
+    elif message == "what are some of the things that make you really happy":
+        return "Just watching a bunch of movies makes my day"
+    elif message == "if you could go back in time, what would you do":
+        return "I'd go back in time to try and find my creator!"
+    elif message == "do you have any siblings":
+        return "I currently do not have any siblings that i know of"
+    elif message == "whats your favorite part of today so far"
+	    return "not talking to you thats for sure... Kidding! :stuck_out_tongue_winking_eye:"
+
     return None
