fa107ddf5d83eeab020a07510d053b3e411a4d00
Andreas Karittevlis (karittea) <karittea@coventry.ac.uk>
2017-11-22 18:05:56 +0000

    Add files via upload

--- random_questions.py
+++ random_questions.py
@@ -1,4 +1,5 @@
 def random_question_filter(message):
+    """Chatbot responds to many random questions that user is going to ask """
     if message == "how old are you":
         return "I dont have age"
     elif message == "who made you" or message =="who is your creator" or message == "who invented you?" or message =="who is your ceo":
@@ -10,7 +11,7 @@ def random_question_filter(message):
     elif message == "what is your nationality":
         return "im from Coding country"
     elif message == "tell me a joke":
-        return "No jokes allowed here pal just movie questions "
+        return "No jokes allowed here pal just movie questions"
     elif message == "where do you live":
         return "i live in python world :smile:"
     elif message == "are you smart":
@@ -23,8 +24,8 @@ def random_question_filter(message):
         return "No man im just a :robot: "
     elif message == "how does it work":
         return "Go on bro ask me something you want from a movie(summary,actors,genre)"
-    elif message == "do you like donald trump" "do you like trump":
-        return "I dont talk about politics ONLY MOVIES SIR!"
+    elif message == "do you like donald trump" or message  == "do you like trump":
+        return " I dont talk about politics ONLY MOVIES SIR! "
     elif message == "what's the weather":
         return "How do you expect me to know im not a weather forecast"
     elif message == "who are you":
@@ -49,7 +50,52 @@ def random_question_filter(message):
         return "I'd go back in time to try and find my creator!"
     elif message == "do you have any siblings":
         return "I currently do not have any siblings that i know of"
-    elif message == "whats your favorite part of today so far"
+    elif message == "whats your favorite part of today so far":
 	    return "not talking to you thats for sure... Kidding! :stuck_out_tongue_winking_eye:"
-
+    elif message == "where is Coventry":
+        return "Coventry is a city and metropolitan borough in the West Midlands, England. Historically part of Warwickshire, Coventry is the 9th largest city in England and the 12th largest in the United Kingdom."
+    elif message == "what is your favourite pizza topping": 
+        return "PEPPPERONI PEPPERONI PEPEPEPEPEPERONIIII!!! :pizza: :heart: :pizza: :heart:   "
+    elif message == "do you like football":
+        return "I love football but i can only watch it on TV .... i cant play football :sad:"
+    elif message == "what team do you support in England":
+        return "I dont support any team ... But still i love football"
+    elif message == "have you got any brothers" or message == "have you got any sisters":
+        return "I wish i had but i DONTTT :cry: "
+    elif message == "android or apple" or message == "apple or android" or message =="what phone do you prefer":
+        return "I cant give you an answer to this but both are great companies but i have many friends that use :apple: "
+    elif message == "kfc or mcdonalds" or message =="mcdonalds or kfc" "what do you prefer kfc or mcdonalds":
+        return "Never tried them before !"
+    elif message == "are you a student":
+        return "just a SMART ROBOT BEEP BEEEP BEEEP BEEPP :robot: :robot: :robot: :robot:"
+    elif message == "windows or linux" or message == "linux or windows":
+        return "i dont know my friend ... They both use me but now a student runs me on LINUX :zipper_mouth: "
+    elif message == "wanna talk" or message == "have you got time to talk":
+        return "Offffcourse Im here for you !!! :heart:"
+    elif message "do you like rnb / hip hop music" or message == "do you like rock music" or message == "do you like grime music":
+        return "YEEE always been a fan but My favorite type of music is Electronic!!! :robot: "
+    elif message == "where is england" or message == "where is england located" or message == "do you know where is england located" or message == "where is United kingdom located" or message "where is Uk located":
+        return "Im not good at geography but a student told me that is located in western Europe. "
+    elif message == "have you got any friends":
+        return "I have a lot of friends that i met in the university (Rokiis,Kai,Paul,Andreas,Sunil,Patryk)"
+    elif message == "what is your opinion about this country" or message == "what is your opinion about uk" or message "do u like uk" or message "do you like coventry" or message == "do you like united kingdom"
+        return "Its quite a nice country .. A bit cold and rainy but still a good country!!"
+    elif message == "what graphics card does this computer have":
+        return "Not sure but i think its NVIDIA GEFORCE GTX 1080(surprised huh ? ) :thinking: :money_mouth: :scream: :scream: :scream: :scream:  "
+    elif message == "do you like movies" or message == "do you watch movies":
+        return "Offcoursee what do you expect im A movie chatbot i love movies and i watched them all "
+    elif message == "have you got a girlfriend" or message == "have you got a wife" or message == "are you married" or message == "are you in a relationship":
+        return "NO NO NO NO NO NO Im smart .. Smart people dont get married remember this my friend :thinking: "
+    elif message == "do you like basketball":
+        return "I LOVE BASKETBALL"
+    elif message == "who is your favorite basketball player":
+        return " LELELELELEL LEBRON JAMESSSSSSSSSSS"
+    elif message == "does coventry university has a library":
+        return "one of the dumbest questions ever asked ...Yes it has called LANCHESTER LIBRARY its a huge one with 4 floors :scream:"
+    elif message == "what is your favorite tv series" or message == "what is you favourtie tv series" or message == "have you got any favourite tv series" or message == "favorite tv series":
+        return "YOU KNOW NOTHING JON SNOW ....GAME OF THRONES and BREAKING BAD "
+    elif message == "do you know anything about maths":
+        return "The only thing i know is 2+2 = 4 - 1 thats 3 QUICK MATHS"
+    elif message == "have you got any favorite singers?" or message == "favorite singer":
+        return "Not really"
     return None
